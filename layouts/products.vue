a10f744492b2c1b6ef8a041eea0174051f522c60
Malik <vofene3689@wlmycn.com>
2024-04-19 15:29:01 +0500

    fixes

--- layouts/products.vue
+++ layouts/products.vue
@@ -10,7 +10,7 @@
     <slot />
   </div>
   <footer class="container mx-auto p-4 flex justify-between border-t-2">
-    <ul>
+    <ul class="flex gap-4">
       <li><NuxtLink to="/">Home</NuxtLink></li>
       <li><NuxtLink to="/about">About</NuxtLink></li>
       <li><NuxtLink to="/products">Products</NuxtLink></li>
